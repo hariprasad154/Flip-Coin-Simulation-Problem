9ac3314d1b2c4f821c2d36e15e2fb887c67618de
Hari Prasad G <hariprasas154@gmail.com>
2022-12-22 20:09:23 +0530

    uc2 is done

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -1,9 +1,30 @@
 #!/bin/bash
 
-output=$((RANDOM %2))
-if [[ $output -eq 1 ]];
-then
-		echo "HEADS"
-else
-		echo "TAILS"
-fi
+head=()
+tail=()
+
+count1=0
+count2=0
+break=0
+while [ $break -lt 21 ]
+do
+
+	output=$((RANDOM %2))
+	if [[ $output -eq 1 ]];
+	then
+			echo "HEADS"
+			head[((count1))]=$count1
+			((count1++))
+		
+	else
+			echo "TAILS"
+			echo "HEADS"
+			tail[((count2++))]=$count2
+			((count2++))
+	fi
+	((break++))
+done
+
+
+echo " the number of time the head won is " ${#head[@]};
+echo " the number of time the tail won is " ${#tail[@]};
