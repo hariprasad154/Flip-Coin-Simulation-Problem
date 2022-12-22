567a19f79b87ed90d3cc338605d9cf48488a2297
Hari Prasad G <hariprasas154@gmail.com>
2022-12-22 20:56:11 +0530

    added

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -3,28 +3,41 @@
 head=()
 tail=()
 
+head_count=$((${#head[@]}));
+tail_count=$((${#tail[@]}));
+
+result=0
+
 count1=0
 count2=0
 break=0
-while [ $break -lt 21 ]
-do
+while 
 
-	output=$((RANDOM %2))
-	if [[ $output -eq 1 ]];
+while [ $result -eq 1]
+do
+	if [[ $(($head_count -0 $tail_count)) -eq 21 ]] 
 	then
-			echo "HEADS"
+			result=1
+	fi
+	output=$((RANDOM %2))
+	elif [[ $output -eq 1 ]];
+	then
+			#echo "HEADS"
 			head[((count1))]=$count1
 			((count1++))
+	fi
 		
-	else
-			echo "TAILS"
-			echo "HEADS"
+	elif
+		#echo "TAILS"
+			
 			tail[((count2++))]=$count2
 			((count2++))
+		
+		((break++))
 	fi
-	((break++))
+
 done
 
 
-echo " the number of time the head won is " ${#head[@]};
-echo " the number of time the tail won is " ${#tail[@]};
+#echo " the number of time the head won is " ${#head[@]};
+#echo " the number of time the tail won is " ${#tail[@]};
